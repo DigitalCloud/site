8ab5667c1563b301f67cf0fbb648092aaa6efbc2
Mohammed Shurrab <devmsh87@gmail.com>
2018-12-24 14:32:52 +0200

    form sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,20 +14,25 @@ const IndexPage = () => (
         <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
             <Image/>
         </div>
-        <form netlify>
-            <div>
-                <label>Name</label>
-                <input name="name"/>
-            </div>
-            <div>
-                <label>Email</label>
-                <input name="email"/>
-            </div>
-            <div>
-                <label>Message</label>
-                <textarea name="message"/>
-            </div>
-            <input type="submit" value="Post"/>
+        <form name="contact" method="POST" data-netlify="true">
+            <p>
+                <label>Your Name: <input type="text" name="name"/></label>
+            </p>
+            <p>
+                <label>Your Email: <input type="email" name="email"/></label>
+            </p>
+            <p>
+                <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                </select></label>
+            </p>
+            <p>
+                <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
         </form>
         <Link to="/page-2/">Go to page 2</Link>
     </Layout>
